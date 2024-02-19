f0fcdbf63257b835eeb16f5569e6ab000599b561
Olly <ollytech92@gmail.com>
2024-02-19 22:32:24 +0000

    complete project 3
    Project 3 complete program

--- 03 - Career Crafter/README.md
+++ 03 - Career Crafter/README.md
@@ -7,13 +7,29 @@ This application was created as part of the Hyperion Dev Software Engineering Bo
 2. Use try-except blocks to gracefully manage unexpected exceptions.
 3. Recognise and address various error types, including edge cases.
 
-The program will take input from a user of name, surname, phone number, location, skills and programming languages. It will validate these inputs and clean the data prior to processing. It will handle predictable user errors as well as edge cases. 
+This program will take input from user related to an application form. These inputs will be validated, and cleaned prior to processing. Predictable user errors and edge cases will be handled. The following inputs will be focused on: full name, phone number, email, age.
 
 As such, this program showcases my knowledge and usage of Python:
 - Conditional statements
 - Try-Except
 - Error Types
 
+### Specific and Additional Features
+Played with the project's objectives to increase challenge and programming learning. Each input was contained in a while loop with a boolean-constrained variable. The inputs were validated and made user-friendly in the following ways:
+- Full Name
+    - used `any` function to identify if integers had been inputted anywhere in string.
+    - identified if only one name had been inputted by searching for whitespace.
+- Age
+    - included parameters for age to showcase terminating programs early when necessary. Individuals outside of the age range will be exited from the program. This will reduce the number of users that input a different age to bypass the restriction.
+    - created error handling for negative numbers as well as non-numbers
+- Email Address
+    - created error handling for email addresses that do not include "@" or ".", or have whitespace in.
+    - inserted a confirmation of email process to ensure users input the correct email.
+- Phone Number
+    - included parameters for numbers that weren't the right length or didn't start with 0 as is typical in the UK. 
+- Confirmation Menu
+    - created this to ensure users had the opportunity to edit prior answers. While loop and Boolean values redirect users in the most efficient way. This means user only has to edit the one portion rather than their whole form.
 
-## Additional Features
+## Additional Features and Future Steps
 - [ ] Allow users to set up their own application conditions that the applicant's data should adhere to.
+- [ ] Refactor code for optimisation and readability
--- 03 - Career Crafter/input_valid.py
+++ 03 - Career Crafter/input_valid.py
@@ -1,33 +1,117 @@
 # user input validations for career application
 
-# name entry and validation 
-while True:
-    user_fullname = input("Enter your full name\t")
-    if " " in user_fullname:
-        user_fullname = user_fullname.split(" ", 1)
-        print("Name accepted.")
-        break
-    else:
-        print("ERROR: please enter a forename and surname")
+# inputs = full name, age, email, phone number - set false variables for while loops
+name = False
+age = False
+email = False
+phone = False
 
-# email address and validation
 while True:
-    user_email = input("Enter your email\t")
-    if "@" in user_email and "." in user_email:
-        print("Email address accepted")
-        break
-    else:
-        print("ERROR: email address invalid. Please try again.")
+    ## full name input and validation
+    while name == False:
+        try:
+            uname = input("Please enter your full name:\t")
+            if any(num.isdigit() for num in uname): #if integers in username
+                raise ValueError("ERROR: Your name must not include digits.")
+            elif " " not in uname: #if full name not inputted.
+                raise ValueError("ERROR: please insert your full name.")
+            else:
+                print(f"Name ({uname.title()}) entered successfully.")
+                name = True
+        except ValueError as ve:
+            print(ve)
 
-# phone number input and validation
-while True:
-    user_phoneno = input("Enter your phone number\t")
-    if len(user_phoneno) == 11 and user_phoneno.isdigit():
-        print("Phone number accepted")
-    else:
-        print("ERROR: invalid number. Please try again.")
-        
+    ## age input and validation
+    while age == False:
+        try:
+            uage = int(input("Enter your age:\t"))
+            if uage < 0: #negative number error fallback
+                raise ValueError
+            elif uage < 18 or uage > 110: #age criteria validation
+                print("Unfortunately, your age does not fit the application's criteria. Thank you for your time.")
+                quit() #stops individual from continuing application or trying to change age
+            else:
+                print(f"Age ({uage}) has been successfully entered.")
+                age = True
+        except ValueError:
+            print("ERROR: Invalid characters entered.")
 
-# user_location = input("Enter your location\t")
-# user_skills = input("Enter your skills\t")
-# user_languages = input("Enter your programming language proficiencies\t")
+    ## email address input and validation      
+    while email == False:
+        try:
+            uemail = input("Enter your email:\t")
+            if " " in uemail:
+                raise ValueError("ERROR: email should not contain spaces. Please try again.")
+            elif "@" in uemail and "." in uemail:
+                # insert email again to confirm it's correctly inputted by user.
+                    confirm_uemail = input("Please confirm your email address:\t")
+                    if uemail == confirm_uemail: #validate both entries match
+                        print(f"Email address ({uemail})  successfully entered")
+                        email = True
+                    else:
+                        raise ValueError("ERROR: Email address does not match. Please try again.")
+            else:
+                raise ValueError("ERROR: Your email address must include '@' and '.' Please try again.")
+        except ValueError as ve:
+            print(ve)
+
+    ## phone number input and validation
+    while phone == False:
+        try:
+            uphone = input("Enter your phone number:\t")
+            if uphone[0] == "0" and len(uphone) == 11 and uphone.isdigit():
+                print(f"Phone number ({uphone}) entered successfully")
+                uphone = int(uphone)
+                phone = True
+            elif not uphone.isdigit():
+                raise ValueError("ERROR: phone number must only contain digits.")
+            elif len(uphone) != 11:
+                raise ValueError("ERROR: phone number must contain 11 digits.")
+            elif uphone[0] != "0":
+                raise ValueError("ERROR: phone number must begin with 0")
+            else:
+                raise ValueError("ERROR: invalid number. Please try again.")
+        except ValueError as ve:
+            print(ve)
+
+    try:
+        confirm_info = input(f"""
+    Please review your application form:
+        Name: {uname.title()}
+        Age: {uage}
+        Email Address: {uemail}
+        Phone Number: {uphone}
+
+    Are these details correct?
+        'y' - yes
+        'n' - no
+
+        """)
+        if confirm_info == "y" or confirm_info == "'y'" or confirm_info == "yes":
+            print("Thank you for entering your information. This program confirms your details pass the minimum  criteria.")
+            break
+        # menu to go back to change information
+        elif confirm_info == "n" or confirm_info == "'n'" or confirm_info == "no":
+            edit_form = int(input("""
+    Which item would you like to edit?
+        1 - Name
+        2 - Age
+        3 - Email Address
+        4 - Phone Number
+                            """))
+            if edit_form == 1:
+                name = False #goes back to earlier name while loop
+            elif edit_form == 2:
+                age = False
+            elif edit_form == 3:
+                email = False
+            elif edit_form == 4:
+                phone = False
+            elif edit_form.isdigit():
+                raise ValueError("ERROR: please select an option from 1-4")
+            else:
+                raise ValueError("ERROR: Please enter a number from the option.")     
+        else:
+            raise ValueError("ERROR: Choice not recognised. Please enter y or n")
+    except ValueError as ve:
+        print(ve)
